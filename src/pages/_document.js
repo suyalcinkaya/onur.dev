6eb1db2a74562fc7eb32a0f31b74b453786f8fa9
Onur Suyalcinkaya <suyalcinkaya@live.com>
2020-05-24 19:14:35 +0300

    dns-prefetch

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,6 +14,7 @@ class MyDocument extends Document {
           <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
           <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+          <link rel="dns-prefetch preconnect" href="https://www.googletagmanager.com" />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
