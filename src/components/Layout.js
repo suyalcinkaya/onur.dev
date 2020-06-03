4cc51ff5677b4b7442876649cfd4075679880f58
Onur Suyalcinkaya <suyalcinkaya@live.com>
2020-06-03 12:22:23 +0300

    Updates y animation

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ import Footer from 'components/Footer'
 const easing = [0.175, 0.85, 0.42, 0.96]
 
 const vars = {
-  exit: { y: 150, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  exit: { y: 20, opacity: 0, transition: { duration: 0.5, ease: easing } },
   enter: { y: 0, opacity: 1, transition: { duration: 0.5, ease: easing } }
 }
 
