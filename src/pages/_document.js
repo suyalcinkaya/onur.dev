df2bcdf7c2d2aceab3925097148eaa35b10ee48c
Onur Suyalcinkaya <suyalcinkaya@live.com>
2020-06-15 16:02:59 +0300

    Updates fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,13 +8,14 @@ class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-          <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
           <link href="/static/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
           <link href="/static/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-          <link rel="dns-prefetch preconnect" href="https://www.googletagmanager.com" />
+          <link rel="preconnect" href="https://www.googletagmanager.com" />
+          <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
           <script
             dangerouslySetInnerHTML={{
@@ -29,6 +30,11 @@ class MyDocument extends Document {
           {/* <style jsx global>
             {globalStyles}
           </style> */}
+          <link rel="preload" href="/fonts/Inter-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Inter-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Inter-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Gilroy-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+          <link rel="preload" href="/fonts/Gilroy-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
         </Head>
         <body>
           <Main />
