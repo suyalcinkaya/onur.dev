bbdbab0889f34a5a30eeb1adbc98584fd9a63220
suyalcinkaya <onur.suyalcinkaya@hey.car>
2021-12-28 23:55:28 +0100

    fix: og image bug

--- src/lib/helper.js
+++ src/lib/helper.js
@@ -1,5 +1,5 @@
 export const ogImageUrl = (title) =>
-  `https://og-image-onur.vercel.app/${encodeURIComponent(title)}.png?md=1&fontSize=125px`
+  `https://og-image-onur.vercel.app/${encodeURIComponent(title)}.png?md=0&fontSize=125px`
 
 export const getReadingTime = (minutes) => (minutes > 1 ? `${Math.ceil(minutes)} mins read` : `1 min read`)
 
