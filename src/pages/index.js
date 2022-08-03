7feacdd4f3b57e3f6a0288b1116410182c0fddab
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2022-08-04 01:16:32 +0200

    chore: add minor improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,6 +77,6 @@ export async function getStaticProps({ preview = false }) {
   const recentPosts = (await getLast3Posts(preview)) ?? []
 
   return {
-    props: { recentPosts }
+    props: { recentPosts, headerTitle: 'Home' }
   }
 }
