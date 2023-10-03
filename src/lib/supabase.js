0f7885dffa7ccd2971455f6dd33c24635bdea1c7
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2023-10-03 19:00:20 +0300

    fix: supabase config

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,20 +1,15 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
   auth: {
     persistSession: false
   }
 })
-export const tableName = 'pages'
+const tableName = 'pages'
 
 export async function getViewCounts() {
   const { data } = await supabase.from(tableName).select('id, slug, view_count')
-
-  return data.map(({ id, slug, view_count }) => ({
-    id,
-    slug,
-    view_count
-  }))
+  return data.map(({ id, slug, view_count }) => ({ id, slug, view_count }))
 }
 
 export async function upsertViewCount(slug) {
@@ -22,21 +17,17 @@ export async function upsertViewCount(slug) {
   const latestData = data[0]
 
   // Don't upsert view count in development or if no slug is provided
-  if (process.env.NODE_ENV !== 'production' || !slug) {
-    return { view_count: latestData?.view_count || 0 }
-  }
+  if (process.env.NODE_ENV !== 'production' || !slug) return { view_count: latestData?.view_count ?? 0 }
 
   const { data: newOrUpdatedData } = await supabase
     .from(tableName)
     .upsert({
       id: latestData?.id,
       slug: slug,
-      view_count: latestData ? latestData.view_count + 1 : 1,
+      view_count: latestData?.view_count ? latestData.view_count + 1 : 1,
       view_count_updated_at: new Date()
     })
     .select()
 
-  return {
-    view_count: newOrUpdatedData[0]?.view_count || 0
-  }
+  return { view_count: newOrUpdatedData[0]?.view_count ?? 0 }
 }
