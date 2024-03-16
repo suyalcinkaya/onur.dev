733e1998afdb221aeef0cec53e6c786559b5692b
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2024-03-16 13:58:41 +0100

    chore: remove lazyOnload strategy from flock.js script tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,7 +54,6 @@ export default async function RootLayout({ children }) {
           src="https://unpkg.com/@tinybirdco/flock.js"
           data-host="https://api.tinybird.co"
           data-token={process.env.NEXT_PUBLIC_TINYBIRD_TOKEN}
-          strategy="lazyOnload"
         />
       </body>
     </html>
