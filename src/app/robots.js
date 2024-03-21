129fe6fbbcbc97f34467688e643190fc0b4e30fa
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2024-03-21 23:39:16 +0100

    feat: create robots.txt file

---
+++ src/app/robots.js
@@ -0,0 +1,10 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/'
+    },
+    sitemap: 'https://onur.dev/sitemap.xml',
+    host: 'https://onur.dev'
+  }
+}
