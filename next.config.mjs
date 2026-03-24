5d7df404d422336bd32c2ff93b0a481054504968
suyalcinkaya <onur.suyalcinkaya@gmail.com>
2026-03-24 17:56:24 +0100

    chore: remove caching for static files

--- next.config.mjs
+++ next.config.mjs
@@ -109,15 +109,6 @@ const nextConfig = {
             value: 'public, max-age=31536000, immutable'
           }
         ]
-      },
-      {
-        source: '/_next/static/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, immutable'
-          }
-        ]
       }
     ]
   },
